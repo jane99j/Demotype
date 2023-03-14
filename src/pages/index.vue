79ed158ea676ae779c3c78ebdd2b2a3bd0691a2e
jane99j <jaynerrch@gmail.com>
2023-03-14 21:41:24 +0700

    new  2 page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Hello, World!</h1>
+  <section class="flex items-center bg-gray-50 dark:bg-gray-900 p-5 sm:p-5">
+    <div class="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+      <!-- Start coding here -->
+      <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+        <div class="flex-row items-center justify-between p-4 space-y-3  sm:space-y-0 sm:space-x-4">
+
+          <img class="h-auto max-w-lg transition-all duration-300 rounded-lg blur-sm hover:blur-none"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png"
+            alt="image description">
+
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
---
+++ src/pages/insetdate.vue
@@ -0,0 +1,80 @@
+<template>
+    
+    <section class="flex items-center bg-gray-50 dark:bg-gray-900 p-5 sm:p-5">
+        <div class="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+            <!-- Start coding here -->
+            <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                <div class="flex-row items-center justify-between p-4 space-y-3  sm:space-y-0 sm:space-x-4">
+
+                    <form>
+                        <div
+                            class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                ลงทะเบียนยานพาหนะ
+                            </h3>
+                        </div>
+
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                                <label for="last_name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ห้องพัก</label>
+                                <select id="countries"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>101</option>
+                                    <option value="US">102</option>
+                                    <option value="US">103</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="last_name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลือกยานพาหนะ</label>
+                                <select id="countries"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected> จักรยานยนต์</option>
+                                    <option value="US">รถยนต์</option>
+                                    <option value="CA">จักรยาน</option>
+                                    <option value="FR">รถตู้</option>
+                                    <option value="DE"> รถตุ๊กตุ๊ก</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="last_name"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ยี่ห้อรถ</label>
+                                <input type="text" id="last_name"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="ระบุยี่ห้อรถ" required>
+                            </div>
+                            <div>
+                                <label for="company"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">สีรถ</label>
+                                <input type="text" id="company"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="ระบุสีรถ" required>
+                            </div>
+                            <div>
+                                <label for="phone"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ทะเบียนรถ</label>
+                                <input type="tel" id="phone"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="เช่น กจ 8672" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
+                            </div>
+                            <div>
+                                <label for="website"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ภาพประกอบ</label>
+
+                                <input
+                                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                    id="file_input" type="file">
+
+
+                            </div>
+
+                        </div>
+                        <button type="submit"
+                            class="text-white mb-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+    </div>
+</section></template>
